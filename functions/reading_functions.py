83e4098b32f7fe85037d60e771e68738a715a8f4
Albatrosfirst <nazm.albatros@gmail.com>
2024-10-16 16:23:40 +0300

    feat: first fitz implementation

--- functions/reading_functions.py
+++ functions/reading_functions.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from datetime import datetime
 import os 
 import fitz
+import re
 
 class ReadingFunctions:
     def __init__(self):
@@ -25,16 +26,29 @@ class ReadingFunctions:
         file_extension = path.suffix.lower()
         try:
             if file_extension == '.pdf':
-                with path.open('rb') as file:
-                    pdf_reader = PyPDF2.PdfReader(file)
+                with fitz.open(path) as file:
                     try:
-                        pdf_date = f"{pdf_reader.metadata.creation_date.year%2000}-{pdf_reader.metadata.creation_date.month}-{pdf_reader.metadata.creation_date.day}"
+                        pdf_date = f"{file.metadata["creationDate"][4:6]}-{file.metadata["creationDate"][6:8]}-{file.metadata["creationDate"][9:11]}"
                         file_data["date"].append(pdf_date)
+                        previous_sentence_count = 0
                     except TypeError as e:
                         raise TypeError(f"PDF creation date could not extracted!: {e}")
-                    for page in pdf_reader.pages:
-                        page_text = page.extract_text()
-                        self._process_text(page_text, file_data)
+                    try: 
+                        for page_num in range(len(file)):
+                            page = file.load_page(page_num)
+                            block_text = page.get_text("blocks")
+                            for block in block_text:
+                                for sent_num in range(len(block[4].split('. '))):
+                                        sentence = re.split(r'(?<=[.!?])\s+', block[4])[sent_num].strip()
+                                        clean_sentence = self._process_regex(sentence)
+                                        if len(clean_sentence) > 15:
+                                            file_data["sentences"].append(clean_sentence)
+                            current_sentence_count = len(file_data["sentences"])
+                            sentences_in_this_page = current_sentence_count - previous_sentence_count
+                            file_data["page_sentence_amount"].append(sentences_in_this_page)
+                            previous_sentence_count = current_sentence_count
+                    except TypeError as e:
+                        raise TypeError(f"PDF text could not extracted!: {e}")
             elif file_extension == '.docx':
                 doc = Document(path)
                 try:
@@ -50,13 +64,23 @@ class ReadingFunctions:
                 text = path.read_text(encoding='utf-8')
                 self._process_text(text, file_data)
             else:
-                raise ValueError(f"Unsupported file type: {file_extension}")
+                raise ValueError(f"Unsupported file type: {file_extension}")        
         except PyPDF2.errors.PdfReadError:
             print(f"Error reading PDF file: {path}. The file might be corrupted or incompatible.")
         except Exception as e:
             print(f"Error reading file: {path}. Error: {str(e)}")
-    
         return file_data
+    
+    def _process_regex(self,text):
+        clean_text = re.sub(r'(\b\w+)\s*\n\s*(\w+\b)',r'\1 \2',text)
+        clean_text = re.sub(r'(\w+)-\s+(\w+)', r'\1\2', clean_text)
+        clean_text = re.sub(r'[,()]\s*\n\s*(\w+)',r' \1',clean_text)
+        clean_text = re.sub(r'(\b\w+)\s*-\s*(\w+\b)',r'\1 \2',clean_text)
+        clean_text = re.sub(r'(\w+)\s*[-–]\s*(\w+)',r'\1\2',clean_text)
+        clean_text = re.sub(r'(?:[\s!\"#$%&\'()*+,\-.:;<=>?@\[\\\]^_`{|}~]+)(?!\w)',r' ',clean_text)
+        clean_text = clean_text.replace('\n','')
+        clean_text = clean_text.replace(' \n','')
+        return clean_text
 
     def _process_text(self, text, file_data):
         docs = self.nlp(text)
